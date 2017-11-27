69b26e17d1959fa59546dc382f8f65fad6d06302
Gbemileke Onilude <lekeonilude@gmail.com>
2017-11-27 14:21:23 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  counter =  0
+  board.each do |element|
+    if element != " " and element != ""
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  counter = turn_count(board)
+  puts counter
+  if counter % 2 == 0
+    puts "X"
+  else
+    puts "O"
+  end
+end
